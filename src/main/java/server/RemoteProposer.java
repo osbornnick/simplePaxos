d59804cc0a27558c68324385a17dab59165edc38
osbornnick <nicholas.p.osborn@gmail.com>
2022-04-10 15:52:43 -0400

    docs

--- src/main/java/server/RemoteProposer.java
+++ src/main/java/server/RemoteProposer.java
@@ -3,16 +3,39 @@ package server;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-/**
- * Performs the Proposer role of the PAXOS algorithm
- */
+/** Performs the Proposer role of the PAXOS algorithm */
 public interface RemoteProposer extends Remote {
 
-    public void setProposal(Operation op) throws RemoteException;
+  /**
+   * Set the current proposal operation
+   *
+   * @param op to set the current proposal too
+   * @throws RemoteException rmi
+   */
+  void setProposal(Operation op) throws RemoteException;
 
-    public void prepare() throws RemoteException;
+  /**
+   * Send a prepare message to all known Acceptors, initiating PAXOS.
+   *
+   * @throws RemoteException rmi
+   */
+  void prepare() throws RemoteException;
 
-    public void receivePromise(int uid, int acceptedProposalID,  Operation acceptedOp) throws RemoteException;
+  /**
+   * Receive a promise message from the given Acceptor
+   *
+   * @param uid of sending Acceptor
+   * @param acceptedProposalID id of last accepted proposal
+   * @param acceptedOp operation of last accepted proposal
+   * @throws RemoteException rmi
+   */
+  void receivePromise(int uid, int acceptedProposalID, Operation acceptedOp) throws RemoteException;
 
-    void setProposalID(int proposalID) throws RemoteException;
+  /**
+   * set the proposal id of the next proposal
+   *
+   * @param proposalID to set
+   * @throws RemoteException rmi
+   */
+  void setProposalID(int proposalID) throws RemoteException;
 }
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -6,6 +6,7 @@ import java.rmi.RemoteException;
 public interface Server extends Remote {
   /**
    * Get and return the value associated with the given key, null if not present
+   *
    * @param key to find value for
    * @return value for key, null if it doesn't exist
    * @throws RemoteException for any number of issues
@@ -14,6 +15,7 @@ public interface Server extends Remote {
 
   /**
    * Put a key value pair in the store
+   *
    * @param key to add
    * @param value to add
    * @return true if committed, false otherwise
@@ -23,6 +25,7 @@ public interface Server extends Remote {
 
   /**
    * Delete a key value pair
+   *
    * @param key to delete from store
    * @return true if committed, false otherwise
    * @throws RemoteException

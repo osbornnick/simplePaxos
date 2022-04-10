0714fc975631c777881e3be4d48e61c2428e753c
osbornnick <nicholas.p.osborn@gmail.com>
2022-04-10 15:59:12 -0400

    formatting

--- src/main/java/server/LearnerImpl.java
+++ src/main/java/server/LearnerImpl.java
@@ -1,58 +1,55 @@
 package server;
 
-
 import logging.Logger;
 
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class LearnerImpl implements RemoteLearner{
+public class LearnerImpl implements RemoteLearner {
 
-    class Proposal {
-        Operation op;
-        int acceptCount;
-        HashSet<Integer> acceptedBy;
+  /** Object representing a proposal, for easy storage of proposals in a map. */
+  class Proposal {
+    Operation op;
+    int acceptCount;
+    HashSet<Integer> acceptedBy;
 
-        Proposal(Operation op) {
-            this.op = op;
-            acceptCount = 0;
-            acceptedBy = new HashSet<>();
-        }
+    Proposal(Operation op) {
+      this.op = op;
+      acceptCount = 0;
+      acceptedBy = new HashSet<>();
+    }
+  }
+
+  private final int quorumSize;
+  private final HashMap<Integer, Proposal> proposals = new HashMap<>();
+  private final ConcurrentHashMap<String, String> dataStore;
+  Logger logger;
+
+  LearnerImpl(Logger logger, int quorumSize, ConcurrentHashMap<String, String> dataStore) {
+    this.quorumSize = quorumSize;
+    this.dataStore = dataStore;
+    this.logger = logger;
+  }
+
+  @Override
+  public void receiveAccepted(int fromUID, int proposalID, Operation op) {
+    Proposal proposal;
+    if (proposals.containsKey(proposalID)) proposal = proposals.get(proposalID);
+    else {
+      proposal = new Proposal(op);
+      proposals.put(proposalID, proposal);
     }
 
-    private final int uid;
-    private final int quorumSize;
-    private HashMap<Integer, Proposal> proposals = new HashMap<>();
-    private ConcurrentHashMap<String, String> dataStore;
-    Logger logger;
-
-    LearnerImpl(int id, Logger logger, int quorumSize, ConcurrentHashMap<String, String> dataStore) {
-        this.uid = id;
-        this.quorumSize = quorumSize;
-        this.dataStore = dataStore;
-        this.logger = logger;
+    if (!proposal.acceptedBy.contains(fromUID)) {
+      proposal.acceptCount++;
+      proposal.acceptedBy.add(fromUID);
     }
 
-    @Override
-    public void receiveAccepted(int fromUID, int proposalID, Operation op) {
-        Proposal proposal;
-        if (proposals.containsKey(proposalID)) proposal = proposals.get(proposalID);
-        else {
-            proposal = new Proposal(op);
-            proposals.put(proposalID, proposal);
-        };
-
-        if (!proposal.acceptedBy.contains(fromUID)) {
-            proposal.acceptCount++;
-            proposal.acceptedBy.add(fromUID);
-        }
-
-        if (proposal.acceptCount == quorumSize) {
-            logger.log("Learning proposal %d", proposalID);
-            op.run(this.dataStore);
-            proposals.clear(); // is this necessary?
-        };
+    if (proposal.acceptCount == quorumSize) {
+      logger.log("Learning proposal %d", proposalID);
+      op.run(this.dataStore);
+      proposals.clear();
     }
-
+  }
 }
--- src/main/java/server/PaxosServerImpl.java
+++ src/main/java/server/PaxosServerImpl.java
@@ -1,6 +1,5 @@
 package server;
 
-
 import java.io.Serializable;
 import java.rmi.RemoteException;
 import java.util.concurrent.ConcurrentHashMap;
@@ -8,101 +7,104 @@ import logging.Logger;
 
 public class PaxosServerImpl implements Server, RemoteProposer, RemoteAcceptor, RemoteLearner {
 
-    int serverID;
-    Courier courier;
-    ConcurrentHashMap<String, String> map;
-    RemoteProposer proposer;
-    RemoteAcceptor acceptor;
-    RemoteLearner learner;
-    private final Logger logger;
+  int serverID;
+  Courier courier;
+  ConcurrentHashMap<String, String> map;
+  RemoteProposer proposer;
+  RemoteAcceptor acceptor;
+  RemoteLearner learner;
+  private final Logger logger;
 
-    PaxosServerImpl(int id, Courier courier) {
-        this.serverID = id;
-        this.courier = courier;
-        this.map = new ConcurrentHashMap<>();
-        logger = new Logger(PaxosServerImpl.class.getName() + id);
-        this.proposer = new ProposerImpl(id, logger, courier, 3);
-        this.acceptor = new AcceptorImpl(id, logger, courier);
-        this.learner = new LearnerImpl(id, logger, 3, map);
-    }
+  PaxosServerImpl(int id, Courier courier) {
+    this.serverID = id;
+    this.courier = courier;
+    this.map = new ConcurrentHashMap<>();
+    logger = new Logger(PaxosServerImpl.class.getName() + id);
+    this.proposer = new ProposerImpl(id, logger, courier, 3);
+    this.acceptor = new AcceptorImpl(id, logger, courier);
+    this.learner = new LearnerImpl(logger, 3, map);
+  }
 
-    @Override
-    public String get(String key) throws RemoteException {
-        logger.log("REQUEST: GET %s", key);
-        return map.get(key);
-    }
+  @Override
+  public String get(String key) throws RemoteException {
+    logger.log("REQUEST: GET %s", key);
+    return map.get(key);
+  }
 
-    @Override
-    synchronized public boolean put(String key, String value) throws RemoteException {
-        logger.log("REQUEST: PUT %s %s", key, value);
-        Operation op = (Operation & Serializable)(map) -> {
-            map.put(key, value);
-            return true;
-        };
-        this.setProposal(op);
-        this.prepare();
-        return true;
-    }
+  @Override
+  public synchronized boolean put(String key, String value) throws RemoteException {
+    logger.log("REQUEST: PUT %s %s", key, value);
+    Operation op =
+        (Operation & Serializable)
+            (map) -> {
+              map.put(key, value);
+              return true;
+            };
+    this.setProposal(op);
+    this.prepare();
+    return true;
+  }
 
-    @Override
-    synchronized public boolean delete(String key) throws RemoteException {
-        logger.log("REQUEST: DELETE %s", key);
-        Operation op = (Operation & Serializable)(map) -> {
-            map.remove(key);
-            return true;
-        };
-        this.setProposal(op);
-        this.prepare();
-        return true;
-    }
+  @Override
+  public synchronized boolean delete(String key) throws RemoteException {
+    logger.log("REQUEST: DELETE %s", key);
+    Operation op =
+        (Operation & Serializable)
+            (map) -> {
+              map.remove(key);
+              return true;
+            };
+    this.setProposal(op);
+    this.prepare();
+    return true;
+  }
 
-    @Override
-    public void setProposal(Operation op) throws RemoteException {
-        this.proposer.setProposal(op);
-    }
+  @Override
+  public void setProposal(Operation op) throws RemoteException {
+    this.proposer.setProposal(op);
+  }
 
-    @Override
-    public void prepare() throws RemoteException {
-        this.proposer.prepare();
-    }
+  @Override
+  public void prepare() throws RemoteException {
+    this.proposer.prepare();
+  }
 
-    @Override
-    public void receivePromise(int uid, int acceptedProposalID,  Operation acceptedOp) throws RemoteException {
-        logger.log("received promise from %d with acceptedProposalID %d", uid, acceptedProposalID);
-        this.proposer.receivePromise(uid, acceptedProposalID, acceptedOp);
-    }
+  @Override
+  public void receivePromise(int uid, int acceptedProposalID, Operation acceptedOp)
+      throws RemoteException {
+    logger.log("received promise from %d with acceptedProposalID %d", uid, acceptedProposalID);
+    this.proposer.receivePromise(uid, acceptedProposalID, acceptedOp);
+  }
 
-    @Override
-    public void setProposalID(int proposalID) throws RemoteException {
-        this.proposer.setProposalID(proposalID);
-    }
+  @Override
+  public void setProposalID(int proposalID) throws RemoteException {
+    this.proposer.setProposalID(proposalID);
+  }
 
-    @Override
-    public void receivePrepare(int fromUID, int proposal) throws RemoteException {
-        this.failAcceptor();
-        this.acceptor.receivePrepare(fromUID, proposal);
-    }
+  @Override
+  public void receivePrepare(int fromUID, int proposal) throws RemoteException {
+    this.failAcceptor();
+    this.acceptor.receivePrepare(fromUID, proposal);
+  }
 
-    @Override
-    public void receiveAcceptRequest(int proposal, Operation op) throws RemoteException {
-        logger.log("Acceptor receiving for proposal %d", proposal);
-        this.failAcceptor();
-        this.acceptor.receiveAcceptRequest(proposal, op);
-    }
+  @Override
+  public void receiveAcceptRequest(int proposal, Operation op) throws RemoteException {
+    logger.log("Acceptor receiving for proposal %d", proposal);
+    this.failAcceptor();
+    this.acceptor.receiveAcceptRequest(proposal, op);
+  }
 
-    @Override
-    public void receiveAccepted(int uid, int proposal, Operation op) throws RemoteException {
-        logger.log("Learner receiving from %s for proposal %d", uid, proposal);
-        this.learner.receiveAccepted(uid, proposal, op);
-    }
+  @Override
+  public void receiveAccepted(int uid, int proposal, Operation op) throws RemoteException {
+    logger.log("Learner receiving from %s for proposal %d", uid, proposal);
+    this.learner.receiveAccepted(uid, proposal, op);
+  }
 
-    /**
-     * with a 1/10 chance, restart this servers acceptor
-     */
-    private void failAcceptor() {
-        if (Math.random() < 0.1) {
-            logger.log("Pseudo-failing Acceptor");
-            this.acceptor = new AcceptorImpl(this.serverID, logger, courier);
-        }
+  /** with a 1/10 chance, restart this servers acceptor */
+  private void failAcceptor() {
+    if (Math.random() < 0.1) {
+      logger.log("Pseudo-failing Acceptor");
+      this.acceptor = new AcceptorImpl(this.serverID, logger, courier);
     }
+  }
 }

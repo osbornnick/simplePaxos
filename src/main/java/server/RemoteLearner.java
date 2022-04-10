16007a0e9538fbdc04de8c2dd3d9028698753fab
osbornnick <nicholas.p.osborn@gmail.com>
2022-04-10 15:54:50 -0400

    docs

--- src/main/java/server/RemoteLearner.java
+++ src/main/java/server/RemoteLearner.java
@@ -3,11 +3,16 @@ package server;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-/**
- * Performs the Learner role of the PAXOS algorithm
- */
+/** Performs the Learner role of the PAXOS algorithm */
 public interface RemoteLearner extends Remote {
 
-    void receiveAccepted(int fromUID, int proposalID, Operation op) throws RemoteException;
-
+  /**
+   * receive an Accepted proposal update from the given Acceptor
+   *
+   * @param fromUID uid of sending Acceptor
+   * @param proposalID id of accepted proposal
+   * @param op accepted operation to apply
+   * @throws RemoteException rmi
+   */
+  void receiveAccepted(int fromUID, int proposalID, Operation op) throws RemoteException;
 }

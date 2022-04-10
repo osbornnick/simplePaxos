696a55053512fef02888c014a3be50862bf49a96
osbornnick <nicholas.p.osborn@gmail.com>
2022-04-10 15:56:50 -0400

    docs

--- src/main/java/server/RemoteAcceptor.java
+++ src/main/java/server/RemoteAcceptor.java
@@ -3,13 +3,24 @@ package server;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-/**
- * Perform the Acceptor role of PAXOS
- */
+/** Perform the Acceptor role of PAXOS */
 public interface RemoteAcceptor extends Remote {
 
-    void receivePrepare(int fromUID, int proposal) throws RemoteException;
+  /**
+   * Receive a prepare message from the given Proposer
+   *
+   * @param fromUID uid of sending proposer
+   * @param proposal id of prepared proposal
+   * @throws RemoteException rmi
+   */
+  void receivePrepare(int fromUID, int proposal) throws RemoteException;
 
-    void receiveAcceptRequest(int proposal, Operation op) throws RemoteException;
-
+  /**
+   * Receive an accept request message from a Proposer
+   *
+   * @param proposal id of accepted proposal
+   * @param op operation of accepted proposal
+   * @throws RemoteException rmi
+   */
+  void receiveAcceptRequest(int proposal, Operation op) throws RemoteException;
 }

37e5921e63020228c71818fed0d271009155d614
osbornnick <nicholas.p.osborn@gmail.com>
2022-04-10 15:44:25 -0400

    docs

--- src/main/java/server/Courier.java
+++ src/main/java/server/Courier.java
@@ -3,78 +3,130 @@ package server;
 import java.rmi.RemoteException;
 import java.util.HashMap;
 
-
-/**
- * Handles groups of servers, sending messages between them
- */
+/** Handles groups of servers, sending messages between them */
 public class Courier {
 
-    HashMap<Integer, RemoteProposer> proposers;
-    HashMap<Integer, RemoteAcceptor> acceptors;
-    HashMap<Integer, RemoteLearner> learners;
+  HashMap<Integer, RemoteProposer> proposers;
+  HashMap<Integer, RemoteAcceptor> acceptors;
+  HashMap<Integer, RemoteLearner> learners;
 
-    Courier() {
-        this.proposers = new HashMap<>();
-        this.acceptors = new HashMap<>();
-        this.learners = new HashMap<>();
-    }
+  /** Initialize a courier with empty lists of proposers, acceptors, and learners */
+  Courier() {
+    this.proposers = new HashMap<>();
+    this.acceptors = new HashMap<>();
+    this.learners = new HashMap<>();
+  }
 
-    public void addProposer(int uid, RemoteProposer p) {
-        this.proposers.put(uid, p);
-    }
+  /**
+   * Add a RemoteProposer with supplied uid to this couriers list of proposer addresses
+   *
+   * @param uid uid of proposer to add
+   * @param p the proposer to add (an RMI stub)
+   */
+  public void addProposer(int uid, RemoteProposer p) {
+    this.proposers.put(uid, p);
+  }
 
-    public void addAcceptor(int uid, RemoteAcceptor a) {
-        this.acceptors.put(uid, a);
-    }
+  /**
+   * Add a RemoteAcceptor with supplied uid to this couriers list of acceptors
+   *
+   * @param uid of acceptor
+   * @param a the acceptor to add (an RMI stub)
+   */
+  public void addAcceptor(int uid, RemoteAcceptor a) {
+    this.acceptors.put(uid, a);
+  }
 
-    public void addLearner(int uid, RemoteLearner l) {
-        this.learners.put(uid, l);
-    }
+  /**
+   * Add a RemoteLearner with supplied uid to this couriers list of learners
+   *
+   * @param uid of learner
+   * @param l the learner to add (an RMI stub)
+   */
+  public void addLearner(int uid, RemoteLearner l) {
+    this.learners.put(uid, l);
+  }
 
-    void sendPrepare(int fromUID, int proposalID) {
-        this.acceptors.forEach((i, a) -> {
-            try {
-                a.receivePrepare(fromUID, proposalID);
-            } catch (RemoteException e) {
-                e.printStackTrace();
-            }
-        });
-    }
-
-    void sendPromise(int toUID, int fromUID, int acceptedProposalID, Operation acceptedOp) {
-        RemoteProposer rp = this.proposers.get(toUID);
-        try {
-            rp.receivePromise(fromUID, acceptedProposalID, acceptedOp);
-        } catch (RemoteException e) {
+  /**
+   * Send a Prepare message to all known RemoteAcceptors
+   *
+   * @param fromUID uid this Prepare message is from
+   * @param proposalID of this associated proposal
+   */
+  void sendPrepare(int fromUID, int proposalID) {
+    this.acceptors.forEach(
+        (i, a) -> {
+          try {
+            a.receivePrepare(fromUID, proposalID);
+          } catch (RemoteException e) {
             e.printStackTrace();
-        }
-
-    }
-    void sendAccept(int proposalID, Operation op) {
-        this.acceptors.forEach((i, a) -> {
-            try {
-                a.receiveAcceptRequest(proposalID, op);
-            } catch (RemoteException e) {
-                e.printStackTrace();
-            }
+          }
         });
-    }
+  }
 
-    void sendAccepted(int fromUID, int proposalID, Operation op) {
-        this.learners.forEach((i, l) -> {
-            try {
-                l.receiveAccepted(fromUID, proposalID, op);
-            } catch (RemoteException e) {
-                e.printStackTrace();
-            }
-        });
+  /**
+   * Send a Promise message to the given proposer
+   *
+   * @param toUID uid of proposer to send promise too
+   * @param fromUID uid of acceptor sending the promise
+   * @param acceptedProposalID the id of the acceptors accepted proposal
+   * @param acceptedOp the operation associated with the acceptors accepted proposal
+   */
+  void sendPromise(int toUID, int fromUID, int acceptedProposalID, Operation acceptedOp) {
+    RemoteProposer rp = this.proposers.get(toUID);
+    try {
+      rp.receivePromise(fromUID, acceptedProposalID, acceptedOp);
+    } catch (RemoteException e) {
+      e.printStackTrace();
     }
+  }
 
-    void updateGlobalProposal(int uidToUpdate, int proposalID) {
-        try {
-            this.proposers.get(uidToUpdate).setProposalID(proposalID);
-        } catch (RemoteException e) {
+  /**
+   * Send an Accept message to each registered Acceptor
+   *
+   * @param proposalID id of proposal being accepted
+   * @param op operation of proposal being accepted
+   */
+  void sendAccept(int proposalID, Operation op) {
+    this.acceptors.forEach(
+        (i, a) -> {
+          try {
+            a.receiveAcceptRequest(proposalID, op);
+          } catch (RemoteException e) {
             e.printStackTrace();
-        }
+          }
+        });
+  }
+
+  /**
+   * Send an accepted message to each registered learner
+   *
+   * @param fromUID the uid of the acceptor sending the message
+   * @param proposalID the proposalID of the accepted proposal
+   * @param op the accepted operation to apply
+   */
+  void sendAccepted(int fromUID, int proposalID, Operation op) {
+    this.learners.forEach(
+        (i, l) -> {
+          try {
+            l.receiveAccepted(fromUID, proposalID, op);
+          } catch (RemoteException e) {
+            e.printStackTrace();
+          }
+        });
+  }
+
+  /**
+   * Update the global proposalID of the specified proposer (when higher ones are seen by acceptors)
+   *
+   * @param uidToUpdate uid of proposer to update
+   * @param proposalID to set the proposalid of next proposal
+   */
+  void updateGlobalProposal(int uidToUpdate, int proposalID) {
+    try {
+      this.proposers.get(uidToUpdate).setProposalID(proposalID);
+    } catch (RemoteException e) {
+      e.printStackTrace();
     }
+  }
 }
--- src/main/java/server/Operation.java
+++ src/main/java/server/Operation.java
@@ -3,7 +3,17 @@ package server;
 import java.io.Serializable;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * An operation defines a change to make to the key-value store. Apply the operation by calling
+ * run()
+ */
 public interface Operation extends Serializable {
 
-    boolean run(ConcurrentHashMap<String, String> on);
+  /**
+   * Commit this operation on the given hashmap
+   *
+   * @param on hashmap to apply the operation too
+   * @return true if the operation succeeded.
+   */
+  boolean run(ConcurrentHashMap<String, String> on);
 }
